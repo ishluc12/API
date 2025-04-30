60700c5001144c8b473918a7b488dee96a417459
ishluc12 <jlucishimwe2003@gmail.com>
2025-04-30 08:33:23 +0200

    third commit

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+require('dotenv').config(); // Load .env
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: parseInt(process.env.DB_PORT) || 5432
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: parseInt(process.env.DB_PORT) || 5432
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    }
+  }
+};
